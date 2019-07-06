4292e185d5b45a18d8a4c690f601f7d98a43af1d
Dmitry Fedorov <dfedorov@meyvndigital.co.uk>
2019-07-07 02:31:46 +0300

    remove excess identation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base  
+class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :messages
