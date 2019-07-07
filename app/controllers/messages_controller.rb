83773bdc2313f485944278329c39e220a2705863
Dmitry Fedorov <dfedorov@meyvndigital.co.uk>
2019-07-08 00:19:31 +0300

    add back-end  for create endpoint

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,11 +6,24 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      # notify channel here
+      head :created
+    else
+      head :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def message_params
+    params.permit(:body)
   end
 end
